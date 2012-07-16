9a93a12bffd2da1a7256e10e15a8cef7f13c8b90
GAURAV <gauravkumar552@gmail.com>
2012-07-16 15:57:34 +0530

    1st version

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<?php include("common.html"); ?>
+    <div id="content"> 
+    <div id="content-left">
+    <p> 
+    Hi all, This is Gaurav Kumar. I just completed final year, M.Sc(tech) Information Systems from BITS pilani.
+    </p>
+    
+    </div>
+    <div id="content-right">
+    </div>
+    </div>
+    
+</div>
+</body>
+</html>
